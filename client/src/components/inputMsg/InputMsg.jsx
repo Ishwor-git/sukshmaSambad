c56ec842235b7be94d4777fa7b807c1151d69099
Ishwor-git <raishwor2@gmail.com>
2024-01-24 21:48:07 +0545

    input message design

---
+++ client/src/components/inputMsg/InputMsg.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./InputMsg.css";
+
+function InputMsg({ onSubmitMsg }) {
+  const [msg, setMsg] = useState("");
+  const handleSubmitButton = (e) => {
+    e.preventDefault();
+    console.log(msg);
+    setMsg("");
+    const date = new Date();
+    const msgObj = {
+      username: "Esor",
+      time: `${date.getHours()}:${date.getMinutes()}`,
+      content: msg,
+      userType: "sender",
+    };
+    onSubmitMsg(msgObj);
+  };
+
+  return (
+    <>
+      <form className="container-msg-imput" onSubmit={handleSubmitButton}>
+        <input
+          className="msg-input"
+          type="text"
+          placeholder="Type your message here"
+          value={msg || ""}
+          onChange={(e) => setMsg(e.target.value)}
+        />
+        <button className="msg-send-btn" type="submit">
+          Send
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default InputMsg;
