b4ce827ace1e37a1bd2a0846e87b6af85552c5ea
Ishwor-git <raishwor2@gmail.com>
2024-01-27 22:50:18 +0545

    sidebar

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,32 @@
+import "./sidebar.css";
+import "https://unpkg.com/boxicons@2.1.4/dist/boxicons.js";
+
+function Sidebar() {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="links home-link">
+          <box-icon name="home" />
+          <span className="link-text">Home</span>
+        </div>
+        <div className="links rooms-link">
+          <box-icon name="chat" />
+          <span className="link-text">Chat rooms</span>
+          <box-icon
+            type="solid"
+            className="dropdown-icon"
+            name="chevrons-right"
+          />
+        </div>
+        <div className="chatroom-list">
+          <div className="chats chatroom-item">
+            <box-icon name="globe" />
+            <span className="link-text">Global</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
