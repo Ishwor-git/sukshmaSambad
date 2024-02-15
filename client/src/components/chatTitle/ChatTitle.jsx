e58ad545d544b79986f6b31afdfd8591cc6a5466
Ishwor-git <raishwor2@gmail.com>
2024-02-15 15:32:00 +0545

    chatTitle update

---
+++ client/src/components/chatTitle/ChatTitle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ChatTitle.css";
+import "https://unpkg.com/boxicons/dist/boxicons.js";
+
+const ChatTitle = () => {
+  return (
+    <>
+      <div className="chat-title">
+        <box-icon name="globe" />
+      </div>
+    </>
+  );
+};
+
+export default ChatTitle;
