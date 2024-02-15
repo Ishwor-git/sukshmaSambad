19dc00dd926dd28edb111bd469eaa17b4a9393f0
Ishwor-git <raishwor2@gmail.com>
2024-02-15 15:33:00 +0545

    turning off sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
 import ChatBox from "./components/chatBox/ChatBox";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <>
-      <div className="app-main">
+      <div
+        className="app"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        {/* <Sidebar /> */}
+
         <ChatBox />
       </div>
     </>
