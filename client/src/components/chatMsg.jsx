106df86fbcd977cc829b607ed3a4351a50f6aabe
Ishwor-git <raishwor2@gmail.com>
2024-03-07 15:03:19 +0545

    finalising chat msg using tailwind

--- client/src/components/chatMsg.jsx
+++ client/src/components/chatMsg.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
-const ChatMsg = () => {
+const ChatMsg = ({ key, username, time, content, userType = "sender" }) => {
+  const theme = {
+    sender: {
+      bg: "bg-emerald-400",
+      align: "self-end",
+    },
+    receiver: {
+      bg: "bg-stone-400",
+      align: "self-start",
+    },
+  };
   return (
-    <div className=" w-fit min-w-40 max-w-80 mx-3 my-2 p-2 flex flex-col ">
-      <div className="w-full bg-stone-400 rounded-t-lg text-xs text- flex flex-row items-center justify-between box-border">
-        <p className="text-gray-200 pl-2">Username</p>
-        <p className="text-gray-200 pr-2">Time</p>
+    <div
+      className={`w-fit min-w-40 ${theme[userType].bg} max-w-80 ${theme[userType].align} mx-3 my-2 rounded-lg flex flex-col `}
+    >
+      <div className="w-full text-xs text- flex flex-row items-center justify-between box-border">
+        <p className="text-gray-200 pl-2">{username}</p>
+        <p className="text-gray-200 pr-2">{time}</p>
       </div>
-      <div className="w-full bg-stone-400 rounded-b-lg flex flex-col">
-        <p className="text-white px-2 pt-0.5 pb-1">Lorem ipsum dolor</p>
+      <div className="w-full">
+        <p className="text-white px-2 pt-0.5 pb-1">{content}</p>
       </div>
     </div>
   );
