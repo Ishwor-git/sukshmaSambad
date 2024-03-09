a13de4b3ebcf350a1cbb93872bd1511e8b1041d3
Ishwor-git <raishwor2@gmail.com>
2024-03-09 16:24:15 +0545

    connected exprexx

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 // hosting server on port 5000
-const io = require("socket.io")(5000, {
+const socketIO = require("socket.io");
+const app = require("express")();
+const http = require("http");
+const path = require("path");
+
+const server = http.createServer(app);
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+const io = socketIO(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/", require(path.join(__dirname, "routes/main.js")));
 
 io.on("connection", (socket) => {
   console.log(` ${socket.id} connected`);
   socket.on("sendMessage", (data) => {
     const temp = { ...data, userType: "receiver" };
-    console.log(temp);
+    // console.log(temp);
     socket.broadcast.emit("receiveMessage", temp);
   });
 });
