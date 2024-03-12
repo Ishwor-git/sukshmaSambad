1aca87b662b7a21fd7057a3c13c7b7ec92cf200d
Ishwor-git <raishwor2@gmail.com>
2024-03-12 17:19:09 +0545

    api call to server for username varification completed

--- client/src/components/innitial.jsx
+++ client/src/components/innitial.jsx
@@ -1,26 +1,64 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
-const Innitial = () => {
+const Innitial = ({ uploadUsername }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [rePassword, setRePassword] = useState("");
   const [labelPass, setLabelPass] = useState({ value: "", theme: "" });
   const [labelUser, setLabelUser] = useState({ value: "", theme: "" });
+  // const [validUser, setValidUser] = useState(null);
+  const [hidePopup, setHidePopup] = useState("");
+
+  const getUsername = async (newUsername) => {
+    const responce = await axios
+      .get(
+        `http://localhost:5000/api/userInfo/checkUser?userName=${newUsername}`
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+    // setValidUser(!responce.data.result);
+    console.log(responce.data, !responce.data.result, newUsername);
+    return !responce.data.result;
+  };
+
+  const postUsername = async (newUsername) => {
+    const responce = await axios
+      .get(`http://localhost:5000/api/userInfo/addUser?userName=${newUsername}`)
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(responce);
+  };
 
   const handleCheckPass = (password1, password2) => {
-    if ((password1 === "") | (password2 === 0)) {
+    if ((password1 === "") | (password2 === "")) {
       setLabelPass({ value: "", theme: "" });
     } else if (password1 === password2) {
-      setLabelPass({ value: "password match", theme: "text-emerald-500" });
+      setLabelPass({ value: "Password match", theme: "text-emerald-600" });
     } else {
-      setLabelPass({ value: "password do not match", theme: "text-rose-600" });
+      setLabelPass({ value: "Password do not match", theme: "text-rose-600" });
+    }
+  };
+
+  const handleSubmit = async () => {
+    const validUser = await getUsername(username);
+    console.log(validUser);
+    if (validUser) {
+      await postUsername(username);
+      setHidePopup("hidden");
+    } else {
+      setLabelUser({ value: "Username already taken", theme: "text-rose-600" });
     }
   };
 
   return (
-    <div className=" w-screen h-screen fixed z-20 bg-white backdrop-blur bg-opacity-10 flex items-center justify-center">
-      <div className=" w-1/3 h-96 bg-white bg-opacity-30 rounded-xl shadow p-6 flex flex-col justify-center items-center">
+    <div
+      className={`${hidePopup} w-screen h-screen fixed z-20 bg-white backdrop-blur bg-opacity-10 flex items-center justify-center`}
+    >
+      <div className=" w-1/3 h-96 bg-white bg-opacity-30 rounded-xl sshadow p-6 flex flex-col justify-center items-center">
         <input
           placeholder="Username"
           value={username}
@@ -67,6 +105,7 @@ const Innitial = () => {
         <button
           type="submit"
           className=" py-3 px-4 rounded-lg m-4 w-32 border-2 font-semibold border-emerald-500 text-emerald-500   hover:bg-emerald-500 hover:text-white transition-all duration-300"
+          onClick={handleSubmit}
         >
           Submit
         </button>
